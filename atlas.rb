2db46bbd4f669fa3a43d32fc8ad4fe9e54ed4c0b
Ícaro Heimig <icaro@icaromh.com>
2014-11-28 11:12:14 -0200

    Removidos includes desnecessários

--- atlas.rb
+++ atlas.rb
@@ -1,9 +1,5 @@
 require 'rubygems' if RUBY_VERSION < "1.9"
 require 'sinatra'
-require 'sinatra/reloader' if development?
-require 'rack/cors'
-require 'odbc_utf8'
-require 'json'
 
 class Atlas < Sinatra::Base
 
